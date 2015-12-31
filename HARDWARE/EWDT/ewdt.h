3343e358664da5635f4c4cfadce940b0c495d120
lisonchen <lison.chen@tkeap.com>
2015-12-31 12:26:21 +0800

    初步添加can\ewdt\led

--- HARDWARE/EWDT/ewdt.h
+++ HARDWARE/EWDT/ewdt.h
@@ -5,7 +5,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
 
-#define EWDT_TOOGLE() 	GPIOC->ODR ^= 0X0001
+#define EWDT_TOOGLE() 	GPIOA->ODR ^= 0X0001
 
 void power_on_bsp_check(void);
 void EWDT_Drv_pin_config(void);
--- PROJECTS/main.c
+++ PROJECTS/main.c
@@ -11,6 +11,9 @@
 #include "delay.h"
 #include "usart.h"
 #include "spi.h"
+#include "can.h"
+#include "led.h"
+#include "ewdt.h"
 
 extern u8 Slave_Temp;
 
@@ -28,13 +31,94 @@ void spi1_test(void)
 
 }
 
+void can_test(void)
+ {	 
+	u8 i=0,t=0;
+	u8 cnt=0;
+	u8 canbuf[8];
+	u8 res;
+        u8 can_rcv;
+	u8 mode=CAN_Mode_LoopBack;//CAN工作模式;CAN_Mode_Normal(0)：普通模式，CAN_Mode_LoopBack(1)：环回模式
+
+	 	
+   
+	CAN_Mode_Init(CAN_SJW_1tq,CAN_BS2_8tq,CAN_BS1_9tq,4,CAN_Mode_LoopBack);//CAN初始化环回模式,波特率500Kbps    
+
+	
+ 	while(1)
+	{
+
+		if(mode==CAN_Mode_LoopBack)//KEY0按下,发送一次数据
+		{
+			for(i=0;i<8;i++)
+			{
+				canbuf[i]=cnt+i;//填充发送缓冲区
+				if(i<4)printf("%s",canbuf[i]);	//显示数据
+				else printf("%s",canbuf[i]);	//显示数据
+ 			}
+			res=Can_Send_Msg(canbuf,8);//发送8个字节 
+			if(res)printf("Failed");		//提示发送失败
+			else printf("OK    ");	 		//提示发送成功								   
+		}else if(mode==CAN_Mode_Normal)//WK_UP按下，改变CAN的工作模式
+		{	   
+//			mode=!mode;
+  			CAN_Mode_Init(CAN_SJW_1tq,CAN_BS2_8tq,CAN_BS1_9tq,4,CAN_Mode_Normal);//CAN普通模式初始化, 波特率500Kbps 
+
+			if(mode==0)//普通模式，需要2个开发板
+			{
+				printf("Nnormal Mode ");	    
+			}else //回环模式,一个开发板就可以测试了.
+			{
+ 				printf("LoopBack Mode");
+			}
+
+		}		 
+		can_rcv=Can_Receive_Msg(canbuf);
+		if(can_rcv)//接收到有数据
+		{			
+			
+ 			for(i=0;i<can_rcv;i++)
+			{									    
+				if(i<4)printf("%s",canbuf[i]);	//显示数据
+				else printf("%s",canbuf[i]);	//显示数据
+ 			}
+		}
+		t++; 
+		delay_ms(10);
+		if(t==20)
+		{
+			LED=!LED;//提示系统正在运行	
+			t=0;
+			cnt++;
+			printf("%d",cnt);	//显示数据
+		}		   
+	}
+}
+
+
 int main(void)
 { 
      NVIC_Configuration();
      
      delay_init();
      
+        //外部看门狗初始化
+//        EWDT_Drv_pin_config();
+//        power_on_bsp_check();
+     
+     
+     
+     
+#if 1     
     spi1_test();
+#else
+  #if 1
+      can_test();
+  #else
+      
+  #endif
+    
+#endif
 
 }
 	   
